d26c9be4cf7a371db1ff81c7e178e620ec1c4005
LaupWing <loc_strict@hotmail.com>
2019-10-04 17:43:51 +0200

    added darsky net api

--- Section6-Section9/weatherApp/app.js
+++ Section6-Section9/weatherApp/app.js
@@ -1,11 +1,6 @@
-console.log('Starting')
-
-setTimeout(()=>{
-    console.log('2 seconds')
-},2000)
-
-setTimeout(()=>{
-    console.log('0 seconds')
-},0)
-
-console.log('Stopping')
+const request = require('request')
+const url = 'https://api.darksky.net/forecast/0bfee81d0d48f12651dd1fc9ef560f04/37.8267,-122.4233?units=si'
+request({url, json:true},(error, response)=>{
+    const data = response.body.currently
+    console.log(data)
+})
