405d82d28a44bc28ba2792b0302d2c4d5f64ea1d
LaupWing <loc_strict@hotmail.com>
2019-10-10 20:06:48 +0200

    beginning with tthe mongodb tutorials

---
+++ Section9-Section16/task-manager/mongodb.js
@@ -0,0 +1,17 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionUrl, {useNewUrlParser:true, useUnifiedTopology:true},(error, client)=>{
+    if(error){
+        return console.log('Unable to connect',error)
+    }
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Laup',
+        age: 24
+    })
+})
