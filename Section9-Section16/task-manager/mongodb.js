b04cdeffc8c5803bc185bc9decb7b87df17873b4
LaupWing <loc_strict@hotmail.com>
2019-10-11 17:22:26 +0200

    update mongodb.js file with insertMany

--- Section9-Section16/task-manager/mongodb.js
+++ Section9-Section16/task-manager/mongodb.js
@@ -1,6 +1,9 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
+const id = new ObjectID()
+console.log(id.getTimestamp())
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
@@ -10,8 +13,33 @@ MongoClient.connect(connectionUrl, {useNewUrlParser:true, useUnifiedTopology:tru
     }
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Laup',
-        age: 24
+    // db.collection('users').insertOne({
+    //     name: 'Laup',
+    //     age: 24
+    // }, (error, result)=>{
+    //     if(error){
+    //         console.log('Unable to insert user')
+    //         console.log(error)
+    //     }else{
+    //         console.log(result.ops)
+    //     }
+    // })
+    db.collection('tasks').insertMany([
+        {
+            task: 'TEST',
+            description: 'HAlllo',
+            completed: false
+        },
+        {
+            task: 'Insertmany',
+            description: 'Inserting many',
+            completed: true
+        },
+    ],(error,result)=>{
+        if(error){
+            console.log(error, 'there is an error')
+        }else{
+            console.log(result.ops)
+        }
     })
 })
