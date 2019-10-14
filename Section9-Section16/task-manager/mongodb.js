0737ddb87c04d808ac32a340e3c24e367a71db5c
LaupWing <loc_strict@hotmail.com>
2019-10-14 16:47:37 +0200

    added validator

--- Section9-Section16/task-manager/mongodb.js
+++ Section9-Section16/task-manager/mongodb.js
@@ -7,25 +7,25 @@ const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 MongoClient.connect(connectionUrl, {useNewUrlParser:true, useUnifiedTopology:true},(error, client)=>{
-    if(error){
-        return console.log('Unable to connect',error)
-    }
-    const db = client.db(databaseName)
-    db
-        .collection('users')
-        .updateOne({
-            _id: new ObjectID("5d9f57ef85817b0b0c664401")
-        },{
-            $set:{
-                name: 'LaupWing'
-            }
-        })
-        .then(result=>{
-            console.log(result)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+    // if(error){
+    //     return console.log('Unable to connect',error)
+    // }
+    // const db = client.db(databaseName)
+    // db
+    //     .collection('users')
+    //     .updateOne({
+    //         _id: new ObjectID("5d9f57ef85817b0b0c664401")
+    //     },{
+    //         $set:{
+    //             name: 'LaupWing'
+    //         }
+    //     })
+    //     .then(result=>{
+    //         console.log(result)
+    //     })
+    //     .catch(err=>{
+    //         console.log(err)
+    //     })
     // db.collection('users').insertOne({
     //     name: 'Laup',
     //     age: 24
--- Section9-Section16/task-manager/src/db/mongoose.js
+++ Section9-Section16/task-manager/src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
+const validator = require('validator')
 
 mongoose.connect(`${connectionUrl}/${databaseName}`, {
     useNewUrlParser:true,
@@ -10,21 +11,52 @@ mongoose.connect(`${connectionUrl}/${databaseName}`, {
 
 const User = mongoose.model('User',{
     name: {
-        type: String
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age:{
         type: Number
     }
 })
 
-const me = new User({
-    name: 'LaupWing',
-    age: 24
+const Task = mongoose.model('Task',{
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }  
 })
-me.save()
-    .then(me=>{
-        console.log(me)
+
+const test = new Task({
+    description:'Testing',
+    completed: false
+})
+
+test.save()
+    .then(test=>{
+        console.log(test)
     })
     .catch(err=>{
         console.log(err)
     })
+// const me = new User({
+//     name: 'LaupWing',
+//     age: 24
+// })
+// me.save()
+//     .then(me=>{
+//         console.log(me)
+//     })
+//     .catch(err=>{
+//         console.log(err)
+//     })
