bc45b7b119a0949e4dc3f8c3f28e36b8cee8cee5
LaupWing <loc_strict@hotmail.com>
2019-10-14 19:11:44 +0200

    little pause

--- Section9-Section16/task-manager/src/index.js
+++ Section9-Section16/task-manager/src/index.js
@@ -1,14 +1,65 @@
 const express = require('express')
 require('./db/mongoose.js') // by just requiring the file you ensure that it runs when you start this file
 const app = express()
+const User = require('./models/user')
+const Task = require('./models/task')
 const port = process.env.PORT || 3000
-
 
 
 app
     .use(express.json()) // use this to parse incoming data to an Object
+    .get('/users', (req,res)=>{
+        User
+            .find({})
+            .then(users=>{
+                res.send(users)
+            })
+            .catch(err=>{
+                res.status(500).send(err)
+            })
+    })
+    .get('/users/:id', (req,res)=>{
+        const _id = req.params.id
+        User
+            .findById(_id)
+            .then(user=>{
+                if(!user){
+                    return res.status(404).send()
+                }
+                res.send(user)
+            })
+            .catch(e=>{
+                res.status(500).send()
+            })
+    })
     .post('/users', (req,res)=>{
-        console.log(req.body)
-        res.send('test')
+        const user = new User(req.body)
+        user
+            .save()
+            .then((user)=>{
+                res
+                    .status(201)
+                    .send(user)
+            })
+            .catch((e)=>{
+                res
+                    .status(400)
+                    .send(e)
+            })
+    })
+    .post('/tasks', (req,res)=>{
+        const task = new Task(req.body)
+        task
+            .save()
+            .then(task=>{
+                res
+                    .status(201)
+                    .send(task)
+            })
+            .catch(e=>{
+                res
+                    .status(400)
+                    .send(e)
+            })
     })
     .listen(port,()=>console.log('app listening to port', port))
