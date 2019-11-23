a21297a416aafd735937cbddfdec32be060c7386
LaupWing <loc_strict@hotmail.com>
2019-11-23 19:53:54 +0100

    added the basic socketfiles

--- Section17/src/index.js
+++ Section17/src/index.js
@@ -1,11 +1,20 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public')
 
 app
     .use(express.static(publicPath))
-    .listen(port,()=>console.log(`Server is listening to port ${port}`))
+
+
+io.on('connection', ()=>{
+    console.log('New websocket connection')
+})
+server.listen(port,()=>console.log(`Server is listening to port ${port}`))
