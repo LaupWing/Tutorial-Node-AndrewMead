c63659db2715ea4c35ac853f07c48985c4a0aa38
LaupWing <loc_strict@hotmail.com>
2019-11-24 18:17:59 +0100

    added teh basic socket events

--- Section17/src/index.js
+++ Section17/src/index.js
@@ -10,11 +10,18 @@ const io = socketio(server)
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public')
 
+let count = 0
 app
     .use(express.static(publicPath))
 
 
-io.on('connection', ()=>{
+io.on('connection', (socket)=>{
     console.log('New websocket connection')
+    socket.emit('countUpdated', count)
+
+    socket.on('increment',()=>{
+        count++
+        io.emit('countUpdated', count)
+    })
 })
 server.listen(port,()=>console.log(`Server is listening to port ${port}`))
