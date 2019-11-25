e97bafa046a87d142c11a607bbeddb671a5655b1
LaupWing <loc_strict@hotmail.com>
2019-11-25 20:56:35 +0100

    added disconnect function

--- Section17/src/index.js
+++ Section17/src/index.js
@@ -19,10 +19,13 @@ io.on('connection', (socket)=>{
     console.log('New websocket connection')
     const message = 'Welcome to the chat app'
     socket.emit('message', message)
-
+    socket.broadcast.emit('message', 'A new user has joined')
     socket.on('sendingMessage',(value)=>{
         console.log(value)
         io.emit('setChat', value)
+    })
+    socket.on('disconnect',()=>{
+        io.emit('message', 'A user has left')
     })
 })
 server.listen(port,()=>console.log(`Server is listening to port ${port}`))
